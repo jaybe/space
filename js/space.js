b8bb0c4211217996c83a4c66ab2737eb923b5a8d
Jonas Bittel <jb.spam@gmx.de>
2012-10-24 23:26:06 +0200

    Add new images and css.

--- js/space.js
+++ js/space.js
@@ -1,4 +1,4 @@
-window.captureEvents(Event.KEYPRESS);
+
 window.onkeydown = keyp;
 window.onkeyup = keyup;
 
@@ -104,19 +104,9 @@ function invaredsShootRelease() {
 	}
 		var invaders2 = bg.getChildren();
 		var idx = parseInt( Math.random() * invaders2.length );
-		var ishot = new Element('div', {
-		'class': 'ishot',
-		id : "ishot",
-		styles: {
-			position: 'absolute',
-			display: 'block',
-			background: 'orange',
-			top: parseInt(invaders2[idx].style.top)+ parseInt(bg.style.top) + 30 + 'px',
-			left: parseInt(invaders2[idx].style.left) + parseInt(bg.style.left) + 13 +"px",
-			width: '4px',
-			height: '10px',
-			//border: '1px solid black'
-		}});
+		var ishot = new Element('div', {'class': 'ishot', id: "ishot"});
+		ishot.setPosition({ y: parseInt(invaders2[idx].getCoordinates().top)+ 30 ,
+							x: parseInt(invaders2[idx].getCoordinates().left) + 13});
 		body.appendChild(ishot);
 		var l = currentlevel;
 		if( currentlevel >= level.length )  {
@@ -136,27 +126,13 @@ function shootRelease() {
 		shotpause = 1;
 		shots[sl] = [];
 		for( var i = 0 ; i < shotlevel ; i++ ) {
-			var shot = new Element('div', {
-				'class': 'shot',
-				styles: {
-					position: 'absolute',
-					display: 'block',
-					background: 'black',
-					top: "250px",
-					left: "30px",
-					width:  '4px',
-					height: '10px',
-					//border: '1px solid black'
-			}});
+			var shot = new Element('div', {'class': 'shot'});
 			if( shotlevel != 2 && i == 0 ) {
-				shot.style.left = parseInt(player.style.left) + 13;
-				shot.style.top = parseInt(player.style.top);
+				shot.setPosition({y: parseInt(player.getCoordinates().top) , x: parseInt(player.getCoordinates().left) + 13 });
 			} else if( (shotlevel == 2 && i == 0 ) || (shotlevel == 3 && i == 1 )) {
-				shot.style.left = parseInt(player.style.left);
-				shot.style.top = parseInt(player.style.top) + 5;
+				shot.setPosition({y: parseInt(player.getCoordinates().top) +5 , x: parseInt(player.getCoordinates().left) });
 			}  else if( (shotlevel == 2 && i == 1) || (shotlevel == 3 && i == 2 ) ) {
-				shot.style.left = parseInt(player.style.left) + 26;
-				shot.style.top = parseInt(player.style.top) + 5;
+				shot.setPosition({y: parseInt(player.getCoordinates().top) +5 , x: parseInt(player.getCoordinates().left) + 26 });
 			}
 			shots[sl][i] = shot;
 			body.appendChild(shot);
@@ -175,20 +151,23 @@ function shoot()  {
 	}
 	if( pause ) {
 		pause = 0;
-		player.style.background = "url(ship.png)";
+		player.removeClass("explode");
+		player.addClass("ship");
 	}
-	//console.log(shot.style.top);
+	//console.log(shot.getCoordinates().top);
 	var child = body.getChildren();
 	for( var i = 0 ; i < child.length; i ++ ) {
 		//console.log(child[i].class);
-		if( child[i].id == "ishot" ) {
-			child[i].style.top = parseInt(child[i].style.top) + 10;
+		if( child[i].hasClass( "ishot" )) {
+			
+			child[i].setPosition({y: parseInt(child[i].getCoordinates().top) + 10});
 			if( detectColision2(child[i]) ) {
 				
 			}
 		}
-		if( child[i].id == "bonus" ) {
-			child[i].style.top = parseInt(child[i].style.top) + 2;
+		if( child[i].hasClass( "bonus" )) {
+			child[i].setPosition({y: parseInt(child[i].getCoordinates().top) + 2});
+			//child[i].getCoordinates().top = parseInt(child[i].getCoordinates().top) + 2;
 			detectBonusPickup(child[i]);
 		}
 	}
@@ -197,7 +176,7 @@ function shoot()  {
 			if( shots[i] ) {
 				var shot = shots[i][x];
 				if( shot ) {
-					shot.style.top = parseInt(shot.style.top) - 10;
+					shot.setPosition({y: parseInt(shot.getCoordinates().top) - 10});
 					if( detectColision(shot) ) {			
 						body.removeChild(shot);
 						shots[i] = removeById(shots[i], x);
@@ -260,19 +239,15 @@ if(bg.getChildren().length == 0) {
 		gameOver = 1;
 	}
 }
-score = new Element('p', {
-'class': 'myClass',
-id: 'scoreLabel',
-html: info,
-});
+score = new Element('p', {id: 'scoreLabel',html: info,});
 scorediv.appendChild(score);
 }
 
 function detectColision2(s) {
-	var top = parseInt(s.style.top);
-//	var left =  parseInt(s.style.left);
-//	var i_left = parseInt(player.style.left);
-	var i_top = parseInt(player.style.top);
+	var top = parseInt(s.getCoordinates().top);
+//	var left =  parseInt(s.getCoordinates().left);
+//	var i_left = parseInt(player.getCoordinates().left);
+	var i_top = parseInt(player.getCoordinates().top);
 	if( top > i_top + 35 ) {
 		var child = body.getChildren();
 		for( var i = 0 ; i < child.length; i ++ ) {
@@ -288,11 +263,12 @@ function detectColision2(s) {
 		lives--;
 		var child = body.getChildren();
 		for( var a = 0 ; a < child.length; a ++ ) {
-			if( child[a].id == "ishot" ) {
+			if( child[a].hasClass( "ishot" ) ) {
 				body.removeChild(child[a]);
 			}
 		}
-		player.style.background = "url(explode.png)";
+		player.addClass("explode");
+		player.removeClass("ship");
 		pause = 1;
 		multi = 1;
 		shotcount = 1;
@@ -307,31 +283,21 @@ function detectColision2(s) {
 }
 function detectColision(shot) {
 	var invaders2 = bg.getChildren();
-	var top = parseInt(shot.style.top);
-	var left =  parseInt(shot.style.left);
+	var top = parseInt(shot.getCoordinates().top);
+	var left =  parseInt(shot.getCoordinates().left);
 	if( top <= -10 ) {
 		multi = 1;
 		updateInfo();
 		return 1;
 	}
 	for( var i = 0 ; i < invaders2.length; i ++ ) {
-		var i_left = parseInt(invaders2[i].style.left) + parseInt(bg.style.left) ;
-		var i_top = parseInt(invaders2[i].style.top) +  parseInt(bg.style.top);
+		var i_left = parseInt(invaders2[i].getCoordinates().left);
+		var i_top = parseInt(invaders2[i].getCoordinates().top);
 		if( i_left <= left &&  (i_left + 30) >= left && i_top <= top &&  (i_top + 30) >= top ) {
 			points = points + 100 * multi;
 			multi++;
-			var bang = new Element('div', {
-			'class': 'boom',
-			styles: {
-				position: 'absolute',
-				display: 'block',
-				background: "url( explode.png)",
-				top: i_top + "px",
-				left: i_left + "px",
-				width:  '30px',
-				height: '30px',
-				//border: '1px solid black'
-			}});
+			var bang = new Element('div', {'class': 'explode'});
+			bang.setPosition({y: i_top , x: i_left });
 			releaseBonus(i, i_top, i_left + 7 );
 			body.appendChild(bang);
 			weg = function() {
@@ -350,18 +316,18 @@ function detectColision(shot) {
 
 function detectBonusPickup(s) {
 	var invaders2 = bg.getChildren();
-	var top = parseInt(s.style.top);
-	var i_top = parseInt(player.style.top);
+	var top = parseInt(s.getCoordinates().top);
+	var i_top = parseInt(player.getCoordinates().top);
 	if( top > i_top + 35 ) {
 		body.removeChild(s);
 		return 1;
 	}
 
 	if( checkColision(player,s)  ) {
-		if( s.style.name == "live" ) {
+		if( s.hasClass( "live" ) ) {
 			lives++;
 		}
-		if( s.style.name == "up" ) {
+		if( s.hasClass( "up" ) ) {
 			shotcount++;
 			if( ( shotcount > shotlevel * 2 ) && shotlevel < 3 ) {
 				shotlevel++;
@@ -370,7 +336,7 @@ function detectBonusPickup(s) {
 			
 			var movespeed = 50
 		}
-		if( s.style.name == "plus" ) {
+		if( s.hasClass( "plus" )) {
 			multi = multi + 10;
 			movespeed = movespeed - 5;
 		}
@@ -383,82 +349,24 @@ function detectBonusPickup(s) {
 
 function releaseBonus(idx, top, left) {
 	var b = bonus[idx];
+		var u = new Element('div', {'id': 'bonus','class':"bonus"});
+		u.setPosition({y: top, x:left});
 	if( b == 1 || b == 30 ) {
-		var u = new Element('div', {
-			'id': 'bonus',
-			styles: {
-				position: 'absolute',
-				display: 'block',
-				name: 'up',
-				background: "url( up.png)",
-				top: top + "px",
-				left: left + "px",
-				width:  '15px',
-				height: '15px',
-				//border: '1px solid black'
-		}});
-		body.appendChild(u);
+		u.addClass("up");
 	} else if( b == 2 ) {
-		var u = new Element('div', {
-			'id': 'bonus',
-			styles: {
-				position: 'absolute',
-				display: 'block',
-				name: 'plus',				
-				background: "url( plus.png)",
-				top: top + "px",
-				left: left + "px",
-				width:  '15px',
-				height: '15px',
-				//border: '1px solid black'
-		}});
-		body.appendChild(u);
+		u.addClass("live");
 	} else if( b == 3 ) {
-		var u = new Element('div', {
-			'id': 'bonus',
-			styles: {
-				position: 'absolute',
-				display: 'block',
-				name: 'live',				
-				background: "url( herz.png)",
-				top: top + "px",
-				left: left + "px",
-				width:  '15px',
-				height: '15px',
-				//border: '1px solid black'
-		}});
-		body.appendChild(u);
+		u.addClass("plus");
 	}
+	body.appendChild(u);
 }
 
 function load() {
-body = document.getElementById("body");
-bg = new Element('div', {
-'class': 'myClass',
-id: 'fieldBG',
-styles: {
-	position: 'absolute',
-	display: 'block',
-	top: "30px",
-	left: "12px",
-	width: (5+30) * startCountX + 'px',
-	height: (5+30) * startCountY + 'px',
-	//border: '1px solid black'
-}});
+body = $("body");
+bg = new Element('div', { 'class': 'fielBG',id: 'fieldBG',});
+//bg.setPosition({width:(5+30) * startCountX ,height:(5+30) * startCountY });
 
-player = new Element('div', {
-	'class': 'myClass',
-	id: 'player',
-	styles: {
-		position: 'absolute',
-		display: 'block',
-		background: "url( ship.png)",
-		top: "375px",
-		left: "30px",
-		width:  '30px',
-		height: '30px',
-		//border: '1px solid black'
-	}});
+player = new Element('div', {'class': 'ship',id: 'player'});
 
 body.appendChild(player);	
 body.appendChild(bg);
@@ -472,8 +380,7 @@ shoot();
 }
 
 function nextLevel () {
-bg.style.top = 20;
-bg.style.left = 10;
+bg.setPosition({x: 20,y:10});
 var count = 0;
 var l = currentlevel;
 if( currentlevel >= level.length )  {
@@ -483,20 +390,8 @@ for( var x = 0; x < level[l].startCountX; x++) {
 	for( var y = 0; y < level[l].startCountY; y++) {
 	bonus[count] = parseInt( Math.random() * level[l].bonus );
 	count++;
-	var invader = new Element('div', {
-	'class': 'invader',
-	id: 'invader' + x + "-" + y,
-	styles: {
-		position: 'absolute',
-		display: 'block',
-		background: "black url(invader.png)",
-		top: y * (30+5) + "px",
-		left: x * (30+5) + "px",
-		width:  '30px',
-		height: '30px',
-		//border: '1px solid black'
-	}});
-
+	var invader = new Element('div', {'class': 'invader'});
+	invader.setPosition({"x":  x * (30+5),"y": y * (30+5) });
 
 	bg.appendChild(invader);
 	}
@@ -511,30 +406,30 @@ function movePlayer () {
 	movePlayer.delay(1000);
 	return;
   }
-  var current_left = parseInt(player.style.left)
-  if( playerMove > 0 && current_left + parseInt(player.style.width) <= parseInt(document.getElementById('playground').style.width) ) {
-	player.style.left = current_left + playerMove + 'px';
+  var current_left = parseInt(player.getCoordinates().left)
+  if( playerMove > 0 && current_left + parseInt(player.getCoordinates().width) <= parseInt($('playground').getCoordinates().width) ) {
+	player.setPosition({x: current_left + playerMove });
   }
   if( playerMove < 0 && current_left > 12 ) {
-	player.style.left = current_left + playerMove + 'px';
+	player.setPosition({x: current_left + playerMove });
   }
-  //console.log(parseInt(bg.style.left) + " " + moveDir);
+  //console.log(parseInt(bg.getCoordinates().left) + " " + moveDir);
   movePlayer.delay(movespeed);
 }
 
  function checkColision(a,b,c) {
-	var al =  parseInt(a.style.left);
-	var at =  parseInt(a.style.top);
-	var ah =  parseInt(a.style.height);
-	var aw =  parseInt(a.style.width);
-	var bl =  parseInt(b.style.left);
-	var bt =  parseInt(b.style.top);
+	var al =  parseInt(a.getCoordinates().left);
+	var at =  parseInt(a.getCoordinates().top);
+	var ah =  parseInt(a.getCoordinates().height);
+	var aw =  parseInt(a.getCoordinates().width);
+	var bl =  parseInt(b.getCoordinates().left);
+	var bt =  parseInt(b.getCoordinates().top);
 	if( c ) {
-		bl +=  parseInt(c.style.left);
-		bt +=  parseInt(c.style.top);
+		bl +=  parseInt(c.getCoordinates().left);
+		bt +=  parseInt(c.getCoordinates().top);
 	}
-	var bh =  parseInt(b.style.height);
-	var bw =  parseInt(b.style.width);
+	var bh =  parseInt(b.getCoordinates().height);
+	var bw =  parseInt(b.getCoordinates().width);
 	if( 
 		(
 			( al < bl && al + aw > bl ) |
@@ -559,45 +454,48 @@ function moveAll () {
   var invaders2 = bg.getChildren();
   var min = 30 * startCountX;
   var max = 0;
-//  var pl =  parseInt(player.style.left);
-//  var pt =  parseInt(player.style.top);
-//  var bl =  parseInt(bg.style.left);
-//  var bt =  parseInt(bg.style.top);
+//  var pl =  parseInt(player.getCoordinates().left);
+//  var pt =  parseInt(player.getCoordinates().top);
+//  var bl =  parseInt(bg.getCoordinates().left);
+//  var bt =  parseInt(bg.getCoordinates().top);
   for( var i = 0 ; i < invaders2.length; i ++ ) {
-	var il = parseInt(invaders2[i].style.left);
-	var it = parseInt(invaders2[i].style.top);
+	var il = parseInt(invaders2[i].getCoordinates().left);
+	var it = parseInt(invaders2[i].getCoordinates().top);
 	if( min > il ) {
 		min = il;
 	}
 	if( max < il ) {
 		max = il;
-		bg.style.width = max +35;
+	//	bg.getCoordinates().width = max +35;
 	}
 
 	if( checkColision(player,invaders2[i],bg) ) {
-		player.style.background = "url(explode.png)";
+		player.addClass("explode");
+		player.removeClass("ship");
 		gameOver = 1;
 		updateInfo();
 	}
  }
 
-  var current_left = parseInt(bg.style.left);
-  if( moveDir > 0 && current_left + max + 32  <=  parseInt(document.getElementById('playground').style.width)) {
-	bg.style.left = current_left + moveDir + 'px';
+  var current_left = parseInt(bg.getCoordinates().left);
+  if( moveDir > 0 && max + 32  <=  parseInt($('playground').getCoordinates().width)) {
+	bg.setPosition({x: current_left + moveDir});
   } else if( moveDir > 0   ) {
 	moveDir = moveDir * -1;
-	bg.style.top = parseInt(bg.style.top) + 10;	
-//	bg.style.top = parseInt(bg.style.top) + 5;
+	bg.setPosition({y: parseInt(bg.getCoordinates().top) + 10});
+	//bg.getCoordinates().top = parseInt(bg.getCoordinates().top) + 10;	
+//	bg.getCoordinates().top = parseInt(bg.getCoordinates().top) + 5;
   }
-  if( moveDir < 0 && current_left + min >= 15 ) {
-	bg.style.left = current_left + moveDir + 'px';
+  if( moveDir < 0 && min + moveDir >= 2 ) {
+	bg.setPosition({x: current_left + moveDir});
   } else if( moveDir < 0   ) {
 	moveDir = moveDir * -1;
-	bg.style.top = parseInt(bg.style.top) + 10;
+	bg.setPosition({y: parseInt(bg.getCoordinates().top) + 10});
+	//bg.getCoordinates().top = parseInt(bg.getCoordinates().top) + 10;
 	//moveAll();
   }
 
-  //console.log(parseInt(bg.style.left) + " " + moveDir);
+  //console.log(parseInt(bg.getCoordinates().left) + " " + moveDir);
 	var l = currentlevel;
 	if( currentlevel >= level.length )  {
 		l = level.length -1;
